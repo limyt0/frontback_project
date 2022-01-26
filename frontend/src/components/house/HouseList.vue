69516788f1103a39bb29578c7618797216613c82
limyt0 <80087763+limyt0@users.noreply.github.com>
2022-01-27 00:55:50 +0900

    file name change

---
+++ frontend/src/components/house/HouseList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+  
+  <div id="map" style="width:100%;height:500px;"></div>
+  <b-container v-if="houses && houses.length != 0" class="bv-example-row mt-3">
+    <house-list-row
+      v-for="(house, index) in houses"
+      :key="index"
+      :house="house"
+    />
+  </b-container>
+  <b-container v-else class="bv-example-row mt-3">
+    <b-row>
+      <b-col><b-alert show>주택 목록이 없습니다.</b-alert></b-col>
+    </b-row>
+  </b-container>
+  
+  </div>
+</template>
+
+<script>
+import HouseListRow from "@/components/house/HouseListRow.vue";
+import { mapState } from "vuex";
+//import google1 from 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+ 
+const houseStore = "houseStore";
+
+
+export default {
+  name: "HouseList",
+  components: {
+    HouseListRow,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(houseStore, ["houses"]),
+    // houses() {
+    //   return this.$store.state.houses;
+    // },
+  },
+  methods: {
+    
+    removeAllChildNods(el) {
+      while (el.hasChildNodes()) {
+        el.removeChild(el.lastChild);
+      }
+    }
+  }
+
+};
+
+
+
+
+</script>
+
+<style></style>
